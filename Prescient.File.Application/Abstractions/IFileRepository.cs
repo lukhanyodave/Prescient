e376a67ca5275ab0904263db1dd27043cc27e916
lukhanyodave <lukhanyokumanda@gmail.com>
2024-05-30 18:32:28 +0200

    client

---
+++ Prescient.File.Application/Abstractions/IFileRepository.cs
@@ -0,0 +1,17 @@
+﻿using Prescient.File.Domain.Models;
+using Prescient.File.Domain.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prescient.File.Application.Abstractions
+{
+    public interface IFileRepository
+    {
+        List<DailyMtm> ProcessFile(string FilePath);
+        bool SaveFile(List<DailyMtm>  filieList);
+        Task DownloadFileAsync(string url, string localFilePath);
+    }
+}
